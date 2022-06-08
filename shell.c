96cec0d841651d9aedbbbc0622f1ebc75d1fe22c
Brighton <101357007+Brighton14@users.noreply.github.com>
2022-06-08 21:36:42 +0100

    shell.h update

--- shell.c
+++ shell.c
@@ -24,24 +24,24 @@ int main(int ac __attribute__((unused)), char *av[], char *envp[])
 			break;
 		if (buffer == NULL)
 			exit(0);
-		av = parse_input_string(buffer);
+		av = parse_input_strn(buffer);
 		if (!av[0])
 		{
 			free(av);
 			continue;
 		}
-		if (_strcmp(av[0], "env") == 0)
+		if (strn_cmp(av[0], "env") == 0)
 		{
-			print_environ(), free(av);
+			print_envn(), free(av);
 			continue;
 		}
-		if (_strcmp(av[0], "exit") == 0)
+		if (strn_cmp(av[0], "exit") == 0)
 			free(av), free(buffer), exit(0);
 		child_pid = fork();
 		if (child_pid == 0)
 		{
-			if (_strchr(av[0], '/') == NULL)
-				av[0] = path_search(av[0]);
+			if (str_char(av[0], '/') == NULL)
+				av[0] = path_srch(av[0]);
 			if (execve(av[0], av, envp))
 			{
 				perror("execve"), exit(EXIT_FAILURE);
