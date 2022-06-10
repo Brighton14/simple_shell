1b2fa911ecdf3f8fc19e80640c0bf023c80707e5
Brighton <101357007+Brighton14@users.noreply.github.com>
2022-06-10 12:59:12 +0100

    Create get_env_var.c

---
+++ test_files/get_env_var.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * get_env_var - retrieves env variable that matches input string
+ * @input: input string
+ * @environ: local environmental variables
+ * Return: string of env variable
+ */
+ 
+char *get_env_var(char *input, char **environ)
+{
+	register int i = 0;
+	char *tok, *right;
+
+	while (environ[i])
+	{
+		tok = _strtok(environ[i], "=");
+		if (_strcmp(tok, input) == 0)
+		{
+			right = _strtok(NULL, "=");
+			return (right);
+		}
+		i++;
+	}
+	return (NULL);
+}
